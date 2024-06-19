708ad3b99687f409f0acc62bdd4f2a07121c4726
wego11ya <wego11ya@gmail.com>
2024-06-19 17:20:57 +0800

    feat(user): add instance method to generate JWT
    - Added a new instance method `createJWT` to the User schema.
    - This method generates a JWT token with user ID and name, expiring in 30 days.
    - Updated the registration controller to use this new method for token generation.

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,9 +5,7 @@ const jwt = require("jsonwebtoken");
 
 const register = async (req, res) => {
   const user = await User.create({ ...req.body });
-  const token = jwt.sign({ userId: user._id, name: user.name }, "jwtSecret", {
-    expiresIn: "30d",
-  });
+  const token = user.createJWT();
   res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 };
 
--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -26,5 +27,11 @@ const UserSchema = new mongoose.Schema({
 UserSchema.pre("save", async function () {
   this.password = await bcrypt.hash(this.password, 10);
 });
+
+UserSchema.methods.createJWT = function () {
+  return jwt.sign({ userId: this._id, name: this.name }, "jwtSecret", {
+    expiresIn: "30d",
+  });
+};
 
 module.exports = mongoose.model("User", UserSchema);
