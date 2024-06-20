618714db6244ddf9bc6f8871fcacb0f63808561b
wego11ya <wego11ya@gmail.com>
2024-06-20 17:31:17 +0800

    feat: add delete job controller

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -47,7 +47,17 @@ const updateJob = async (req, res) => {
 };
 
 const deleteJob = async (req, res) => {
-  res.send("delete job");
+  const {
+    body: { company, position },
+    user: { userId },
+    params: { id: jobId },
+  } = req;
+
+  const job = await Job.findOneAndDelete({ _id: jobId, createdBy: userId });
+  if (!job) {
+    throw new NotFoundError(`No job with id : ${jobId}`);
+  }
+  res.status(StatusCodes.OK).send();
 };
 module.exports = {
   getAllJobs,
