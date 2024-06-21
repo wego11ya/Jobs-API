0db78650061e9709211f083b3da784880d92e643
wego11ya <wego11ya@gmail.com>
2024-06-21 11:29:40 +0800

    feat: handle Mongoose duplicate key error for 'email' field in User model

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,10 +1,24 @@
-const { CustomAPIError } = require('../errors')
-const { StatusCodes } = require('http-status-codes')
+const { CustomAPIError } = require("../errors");
+const { StatusCodes } = require("http-status-codes");
 const errorHandlerMiddleware = (err, req, res, next) => {
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message })
-  }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
-}
+  let customError = {
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || "Something went wrong, please try again",
+  };
 
-module.exports = errorHandlerMiddleware
+  if (err instanceof CustomAPIError) {
+    return res.status(err.statusCode).json({ msg: err.message });
+  }
+  // Handle Mongoose duplicate key error for 'email' field in User model
+  if (err.code && err.code === 11000) {
+    customError.statusCode = StatusCodes.BAD_REQUEST;
+    // customError.msg = "email must be unique";
+    customError.msg = `${Object.keys(
+      err.keyValue
+    )} must be unique, please choose another value`;
+  }
+  // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
+  return res.status(customError.statusCode).json({ msg: customError.msg });
+};
+
+module.exports = errorHandlerMiddleware;
